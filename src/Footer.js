b872bd5fab989e979a6c5572d5a33d02c66ed4f5
Your Name <ebelinggianmarco@gmail.com>
2021-02-23 15:14:11 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2021 Airbnb clone by <a href="https://www.linkedin.com/in/gianmarco-ebeling-856020135/">5ebs</a>. No rights reserved (it's a clone)</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Footer;
